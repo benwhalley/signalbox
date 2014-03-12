03b0be78418fa4a901271c3f2cb18b0f9cb1b2e1
Ben Whalley <benwhalley@gmail.com>
2014-03-12 12:24:08 +0000

    added debugging for superusers when debug=0

---
+++ signalbox/middleware/superuser.py
@@ -0,0 +1,8 @@
+from django.views.debug import technical_500_response
+import sys
+from django.conf import settings
+
+class UserBasedExceptionMiddleware(object):
+    def process_exception(self, request, exception):
+        if request.user.is_superuser or request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
+            return technical_500_response(request, *sys.exc_info())
